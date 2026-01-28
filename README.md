a575c869d35a0541ab467993035cf04c8bc340a1
Devang Kartik <devangkartiksuryavanshi@gmail.com>
2026-01-29 00:30:30 +0530

    play with routes

---
+++ README.md
@@ -0,0 +1,2 @@
+Advance Routing technica : rejex/a/ , + , * , ()
+//localhost:7777/user?userId=23432&userName="Devang kartik" =  access (req.query)
--- src/app.js
+++ src/app.js
@@ -2,14 +2,28 @@ const express = require('express')
 
 const app =  express()
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello Hello Hello")
+
+// app.use("/",(req,res)=>{
+//     res.send("I'm GOAT Use Methode which is override on everything")
+// });
+
+app.get("/user/:userId/:name",(req,res)=>{
+    // console.log(req.query)
+    console.log(req.params)
+    res.send({getName : "mohit"})
 });
 
-app.use("/",(req,res)=>{
-    res.send("welcome your server is started")
+app.post("/user",(req,res)=>{
+    res.send("Data is updated sucessfully")
 });
 
+app.delete("/user",(req,res)=>{
+    res.send("Data is deleted sucessfully")
+});
+
+app.use("/test ",(req,res)=>{
+    res.send("This is test Api ")
+});
 
 app.listen(7777,()=>{
     console.log("server is started my friend")
